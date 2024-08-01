2861e119b0615b7b2b113a4e48e5be8516e6aefe
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-01 14:35:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,19 @@
 #!/usr/bin/env node
 'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const authenticate = require('./src/authenticate');
+const app = require('express')();
 const params = require('./src/params');
 const compress = require('./src/compress');
-const axios = require('axios');
+const bypass = require('./src/bypass');
+const shouldCompress = require('./src/shouldCompress');
 
-const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-
-app.post('/compress', authenticate, params, async (req, res) => {
-  try {
-    const response = await axios.get(req.params.url, { responseType: 'arraybuffer' });
-    const buffer = Buffer.from(response.data);
-
-    // Set necessary params for compress function
-    req.params.originSize = buffer.length;
-    req.params.originType = response.headers['content-type'];
-
-    compress(req, res, buffer);
-  } catch (error) {
-    // Fallback to sending the original image if fetching or compression fails
-    if (error.response) {
-      // Error with the response from the original URL
-      res.setHeader('content-type', error.response.headers['content-type']);
-      res.setHeader('content-length', error.response.data.length);
-      res.status(200).send(Buffer.from(error.response.data));
-    } else {
-      // General error
-      res.status(500).send('Error fetching or compressing the image');
-    }
+app.get('/compress', params, (req, res) => {
+  if (shouldCompress(req)) {
+    compress(req, res);
+  } else {
+    bypass(req, res, req.params.buffer);
   }
 });
-
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
