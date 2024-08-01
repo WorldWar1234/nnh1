523bf82bf0a8c7f8ad4cbb6f234179fa8a4676ca
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-01 15:21:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 #!/usr/bin/env node
 'use strict';
-const app = require('express')();
+const express = require('express');
+const request = require('request');
+const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const compress = require('./src/compress');
-const bypass = require('./src/bypass');
 const shouldCompress = require('./src/shouldCompress');
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/compress', params, (req, res) => {
-  if (shouldCompress(req)) {
-    compress(req, res);
-  } else {
-    bypass(req, res, req.params.buffer);
-  }
+app.enable('trust proxy');
+
+app.get('/', authenticate, params, (req, res) => {
+    const url = req.params.url;
+
+    // No need to check for URL here; handled in params.js
+    // Fetch the image from the URL
+    request.get({ url, encoding: null }, (err, response, buffer) => {
+        if (err || response.statusCode >= 400) {
+            return res.status(500).send('Error fetching the image.');
+        }
+
+        req.params.originType = response.headers['content-type'] || '';
+        req.params.originSize = buffer.length;
+
+        if (shouldCompress(req)) {
+            compress(req, res, buffer);
+        } else {
+            res.setHeader('content-type', req.params.originType);
+            res.setHeader('content-length', buffer.length);
+            res.status(200).send(buffer);
+        }
+    });
 });
+
 app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
