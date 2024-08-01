7d9b306adf513e2c1c774d704f21a25ef1515ecb
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-01 15:26:03 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,51 +1,23 @@
 const DEFAULT_QUALITY = 40;
-const request = require('request');
-const pick = require('lodash').pick;
 
 function params(req, res, next) {
-  const { url, jpeg, bw, l } = req.query;
+    const { url, jpeg, bw, l } = req.query;
 
-  if (!url) {
-    return res.end('bandwidth-hero-proxy');
-  }
-
-  const urls = Array.isArray(url) ? url.join('&url=') : url;
-  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-
-  req.params.url = cleanedUrl;
-  req.params.webp = !jpeg;
-  req.params.grayscale = bw !== '0';
-  req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
-
-  // Fetch the image and store the buffer in the request parameters
-  request.get(
-    req.params.url,
-    {
-      headers: {
-        ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-        'user-agent': 'Bandwidth-Hero Compressor',
-        'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        via: '1.1 bandwidth-hero'
-      },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) {
-        return res.status(origin.statusCode).end();
-      }
-
-      req.params.originType = origin.headers['content-type'] || '';
-      req.params.originSize = buffer.length;
-      req.params.buffer = buffer;
-
-      next();
+    // Check if URL parameter is missing
+    if (!url) {
+        return res.end('bandwidth-hero-proxy');
     }
-  );
+
+    // Process parameters if URL is present
+    const urls = Array.isArray(url) ? url.join('&url=') : url;
+    const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+
+    req.params.url = cleanedUrl;
+    req.params.webp = !jpeg;
+    req.params.grayscale = bw !== '0';
+    req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
+
+    next();
 }
 
 module.exports = params;
