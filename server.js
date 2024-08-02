530a4e9468d63e3c52efa891c0ae2c88bc77c0e3
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-02 16:04:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,32 +6,30 @@ const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const compress = require('./src/compress');
 const shouldCompress = require('./src/shouldCompress');
-const redirect = require('./src/redirect'); // Import the redirect function
+const bypass = require('./src/bypass');
+const redirect = require('./src/redirect');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.enable('trust proxy');
 
 app.get('/', authenticate, params, (req, res) => {
     const url = req.params.url;
 
     // Fetch the image from the URL
-    request.get({ url, encoding: null }, (err, response, buffer) => {
-        if (err || response.statusCode >= 400) {
+    request.get({ url, encoding: null }, (err, origin, buffer) => {
+        if (err || origin.statusCode >= 400) {
             // Use the redirect function if there is an error
             return redirect(req, res);
         }
 
-        req.params.originType = response.headers['content-type'] || '';
+        req.params.originType = origin.headers['content-type'] || '';
         req.params.originSize = buffer.length;
 
         if (shouldCompress(req)) {
             compress(req, res, buffer);
         } else {
-            res.setHeader('content-type', req.params.originType);
-            res.setHeader('content-length', buffer.length);
-            res.status(200).send(buffer);
+            // Bypass compression and directly send the original image
+            bypass(req, res, buffer);
         }
     });
 });
