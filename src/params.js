ce3c45f4b084ab5816972c9a197b5faa2a97117d
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-02 17:00:41 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -9,10 +9,7 @@ function params(req, res, next) {
     }
 
     // Process parameters if URL is present
-    const urls = Array.isArray(url) ? url.join('&url=') : url;
-    const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-
-    req.params.url = cleanedUrl;
+    req.params.url = decodeURIComponent(url);
     req.params.webp = !jpeg;
     req.params.grayscale = bw !== '0';
     req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
